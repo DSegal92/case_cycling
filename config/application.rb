9bfef2464dd62741b42c5a7ad5b2468d151f4869
Daniel Segal <DSegal92@gmail.com>
2012-10-11 01:55:08 -0400

    Pushing stopped for a second... weird

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module CaseCycling
   class Application < Rails::Application
 
     config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
-        r301 %r{.*}, 'http://ww.casecycling.com$&', :if => Proc.new {|rack_env|
+        r301 %r{.*}, 'http://www.casecycling.com$&', :if => Proc.new {|rack_env|
         rack_env['SERVER_NAME'] == 'casecycling.com'
         }
     end
