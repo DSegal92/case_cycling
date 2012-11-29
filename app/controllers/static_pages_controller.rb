a829821f7c9568f39fa8e871f4641f26d1fd8165
Daniel Segal <DSegal92@gmail.com>
2012-11-28 22:52:50 -0500

    Fixed time zone issue with 'TZ=America/Detroit' and fixed rides this week

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
 
-    @rides = Ride.find(:all).select{|g| (g.time.wday - Time.now.wday > 0)}
+    @rides = Ride.find(:all).select{|g| (g.time.wday - Time.now.wday >= 0)}
   	@events = Event.find(:all, :order => "date DESC").first(3)
   	@posts = Blog.find(:all, :order => "date DESC").first(2)
   	@carousel = Carousel.where(:enabled => true)
