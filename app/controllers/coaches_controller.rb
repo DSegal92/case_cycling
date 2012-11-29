7a15281bc28dc177bdf8201bf359be86a2c5fac2
Daniel Segal <DSegal92@gmail.com>
2012-11-28 23:11:29 -0500

    Fixed coaches order

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -1,2 +1,12 @@
-class CoachesController < InheritedResources::Base
-end
+class CoachesController < ApplicationController
+  # GET /members
+  # GET /members.json
+  def index
+    @coaches = Coach.find(:all, :order => "id DESC")
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @members }
+    end
+  end
+ end
