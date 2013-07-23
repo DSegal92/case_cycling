264501569d8773e490e684be431ce71f08c75a28
Daniel Segal <DSegal92@gmail.com>
2013-07-23 14:42:25 -0400

    Added new officer positions

--- app/models/officer.rb
+++ app/models/officer.rb
@@ -2,7 +2,7 @@ class Officer < ActiveRecord::Base
   attr_accessible :bio, :contact, :majors, :minors, :name, :picture, :position, :year, :position_id
   has_many :positions
 
-  def getPositions
-  	types = ["President","Vice President","Treasurer","Safety Officer", "Road Race Coordinator", "Sponsorship Coordinator", "Webmaster"]
-  end
+ 	 def getPositions
+  		types = ["President","Vice President","Treasurer","Secretary","Webmaster","Race Director","Travel Coordinator","Safety Officer"]
+	end
 end
